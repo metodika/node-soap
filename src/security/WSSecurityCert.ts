42dda253782b7eb7e42675bd66888be06e60986b
metodikamikaelnilsson <mikael@metodika.com>
2025-04-01 14:55:51 +0200

    Update WSSecurityCert.ts

--- src/security/WSSecurityCert.ts
+++ src/security/WSSecurityCert.ts
@@ -9,7 +9,7 @@ function addMinutes(date: Date, minutes: number) {
 function dateStringForSOAP(date: Date): string {
   return date.getUTCFullYear() + '-' + ('0' + (date.getUTCMonth() + 1)).slice(-2) + '-' +
     ('0' + date.getUTCDate()).slice(-2) + 'T' + ('0' + date.getUTCHours()).slice(-2) + ':' +
-    ('0' + date.getUTCMinutes()).slice(-2) + ':' + ('0' + date.getUTCSeconds()).slice(-2) + 'Z';
+    ('0' + date.getUTCMinutes()).slice(-2) + ':' + ('0' + date.getUTCSeconds()).slice(-2) + '.001Z';
 }
 
 function generateCreated(): string {
